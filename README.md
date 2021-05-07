35bcb72b08dd75849bd09f66ce973d200a741606
Şamil Servet Avcı <bekekgaj@gmail.com>
2021-05-07 15:17:46 +0300

    proje tamamlandı

---
+++ README.md
@@ -0,0 +1,31 @@
+## About The Project
+rating for web pages
+![](page.gif)
+
+
+### Built With
+
+* [CSS](https://html.com/)
+* [HTML](https://html.com/)
+* [PHP](https://www.php.net/)
+
+
+
+## Getting Started
+###((En)
+You can download the files and arrange them according to your own.
+I wrote down the way of working in it index.php.
+
+
+###(TR)
+İndirip kendinize göre şekillendirebilirsiniz.
+index.php içinde detyalı şekilde yazdım çalışma şeklini.
+
+### Installation
+1. Clone the repo
+   ```sh
+   git clone https://github.com/baradielbenimulan/PHP-Sayfalama.git
+   ```
+
+## Contact
+Şamil Servet Avcı - [@samilservet](https://twitter.com/samilservet) - bekekgaj@gmail.com
--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-try{
-	$veritabani_baglanti = new PDO("mysql:host=localhost;dbname=sayfalama;charset=UTF8", "root", "");
-}catch(PDOException $hata){
-	echo "Bağlantı sorunu<br />".$hata->getMessage();
-	die();
-}
+require_once("conn.php");
 
 if (isset($_REQUEST["sayfa"])){
 	$gelen_sayfa = $_REQUEST["sayfa"];
@@ -14,11 +9,11 @@ if (isset($_REQUEST["sayfa"])){
 
 $sayfalama_buton_sayisi = 2;
 /* sayfa butonlarında sol ve sağ kısımların kaça kadar gitmesi gerektiğini ayarlar
-örnek: sayfa 3'deyiz
-3 4 5 6 7
+örnek: sayfa 5'deyiz
+<< < 3 4 5 6 7 > >>
 gibi
 */
-$sayfa_basina_gosterilecek_kayit_sayisi = 5;
+$sayfa_basina_gosterilecek_kayit_sayisi = 3;
 /* bir sayfada kaç ürün göstersin */
 
 
@@ -29,7 +24,7 @@ $toplam_kayit_sayisi = $toplam_kayit_sayisi_sorgusu->rowCount();
 
 $sayfalamaya_baslanacak_kayit_sayisisi = ($gelen_sayfa*$sayfa_basina_gosterilecek_kayit_sayisi)-($sayfa_basina_gosterilecek_kayit_sayisi);
 /*
-burada hesaplma yaptık
+burada hesaplama yaptık
 eğer sayfa değeri gelmezse ilk sayfayı aç
 eğer sayfa değeri gelirse gelen sayfa değerini aç
 */
@@ -54,17 +49,20 @@ ve ona göre altta sayfalar görünecek
 <body>
 <div class="urunler">
 <?php
+/* burada gelen urunleri veritabanından çekeceğiz. Çekerken hazırladığımız limiitte çekiyoruz. */
+
 $kayit_sorgusu = $veritabani_baglanti->prepare("SELECT * FROM urunler ORDER BY id ASC LIMIT $sayfalamaya_baslanacak_kayit_sayisisi, $sayfa_basina_gosterilecek_kayit_sayisi");
 $kayit_sorgusu->execute();
 $kayit_sayisi = $kayit_sorgusu->rowCount();
 $kayit_degerleri = $kayit_sorgusu->fetchAll(PDO::FETCH_ASSOC);
 
+/* burada çektiğimiz kayıtları ilgili alanlara sıralı şekilde yazması için foreech oluşturduk */
 foreach ($kayit_degerleri as $kayitlar) {
 	?>
 	<div class="icerik">
 		<img class="rsm" src="img/<?php echo($kayitlar['UrunResmi']); ?>">
 		<h5 class="baslik-h5"><?php echo $kayitlar["UrunAdi"]; ?></h5>
-		<p class="icerik_text"><?php echo $kayitlar["UrunFiyati"]." ".$kayitlar["ParaBirimi"]; ?></p>
+		<p class="icerik-fiyat"><?php echo $kayitlar["UrunFiyati"]." ".$kayitlar["ParaBirimi"]; ?></p>
 		<a href="#" class="devami_oku" >Satın Al</a>
 	</div>
 <?php
@@ -73,10 +71,12 @@ foreach ($kayit_degerleri as $kayitlar) {
 </div>
 <div class="sayfalama-kapsayici">
 	<div class="sayfalama-metin">
+		<!-- gelen değerlerle sayfa ve ürün sayılarını yazdık -->
 		Toplam <?php echo $bulunan_sayfa_sayisi; ?> sayfada <?php echo $toplam_kayit_sayisi; ?> ürün bulunmuştur
 	</div>
 	<div class="sayfalama-buton">
 	<?php
+	// eğer 1. dışındaki bir sayfadaysa burayı çalıştır
 		if ($gelen_sayfa>1) {
 			echo '<span class="pasif"><a href="index.php?sayfa=1"> << </a></span>';
 			$sayfayi_bir_geri_al = $gelen_sayfa-1;
@@ -90,14 +90,29 @@ foreach ($kayit_degerleri as $kayitlar) {
 
 		/* sayfa değerlerini gösteren for döngüsü */
 		for (($sayfa_index_degeri=$gelen_sayfa-$sayfalama_buton_sayisi); ($sayfa_index_degeri<=$gelen_sayfa+$sayfalama_buton_sayisi); $sayfa_index_degeri++){
+			/*
+			burada şöyle oldu örnek $gelen_sayfa = 4; diyelim: ($sayfalama_buton_sayisi = 2;'ydi (10.satıra bak))
+
+			$sayfa_index_degeri= 4 - 2;
+			$sayfa_index_degeri = 2; oldu ve
+
+			$sayfa_index_degeri= 4 + 2;
+			$sayfa_index_degeri= 6; oldu
+
+			her döngüde $sayfa_index_degeri'ini arttırdık. ve sonuç:
+								2 3 4 5 6
+			oldu
+
+			*/
 
 			if (($sayfa_index_degeri>0) and ($sayfa_index_degeri<=$bulunan_sayfa_sayisi)){
-				/* eğer kullanıcı 1. sayfdaysa 0 ve -1 sayılarını gösterme*/
+				/* eğer kullanıcı 1. sayfadaysa 0 ve -1 sayılarını gösterme*/
 				/* ya da son sayfaya giderse olmayan sayfa değerlerini gösterme*/
 				if ($sayfa_index_degeri==$gelen_sayfa){
 					echo " ".'<span class="aktif">'.$sayfa_index_degeri.'</span>'." ";
 					
 				}else{
+					/* eğer farklı sayfadaysa */
 					echo '<span class="pasif"><a href="index.php?sayfa='.$sayfa_index_degeri.'">'.$sayfa_index_degeri.'</a></span>';
 				}
 				
